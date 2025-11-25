ee90714f62dafa9cad49ea17374a7c8704ec9058
Satria Arya Diva <satriaarya125@gmail.com>
2025-11-25 14:05:16 +0700

    configurasi env firebase

---
+++ lib/firebaseAdmin.ts
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://primko-b0a09-default-rtdb.firebaseio.com",
+  });
+}
