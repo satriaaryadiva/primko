f53d54bfc6810513749b55bef7bb7ed1c976ff72
Satria Arya Diva <satriaarya125@gmail.com>
2025-12-19 04:46:59 +0700

    ADD ICON

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,8 @@ export const metadata: Metadata = {
     title: "Primko",
   },
   icons: {
-    icon: "/icon0.svg",
+    icon: "/favicon.ico",
+    
     apple: "/icon-192x192.png",
   },
 };
@@ -25,7 +26,12 @@ export default function RootLayout({
   return (
     <html lang="id">
       <head>
-
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="PRIMKO" />
+<link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#155dfc" />
